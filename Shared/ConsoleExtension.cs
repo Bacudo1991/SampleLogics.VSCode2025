35e7dfccccd4b77f2eeb7f563006c5b72b435dc8
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-08-22 19:31:01 -0500

    Complete exercice 6

--- Shared/ConsoleExtension.cs
+++ Shared/ConsoleExtension.cs
@@ -6,10 +6,41 @@ public class ConsoleExtension
     {
         Console.Write(message);
         var input = Console.ReadLine();
-        if (int.TryParse(input, out int result))
+        if (int.TryParse(input, out int numberInt))
         {
-            return result;
+            return numberInt;
         }
         throw new Exception("El valor ingresado no es un número entero válido.");
+    }
+    
+        public static float GetFloat(string message)
+    {
+        Console.Write(message);
+        var numberString = Console.ReadLine();
+        if (float.TryParse(numberString, out float numberFloat))
+        {
+            return numberFloat; 
+        }
+        throw new Exception("El valor ingresado no es válido.");
+    }
+
+        public static decimal GetDecimal(string message)
+    {
+        Console.Write(message);
+        var numberString = Console.ReadLine();
+        if (decimal.TryParse(numberString, out decimal numberDecimal))
+        {
+            return numberDecimal;  
+        }
+        throw new Exception("El valor ingresado no es válido.");
+    }
+
+    public static String? GetString(string message)
+    {
+        Console.Write(message);
+        var text = Console.ReadLine();
+        return text; ;
+
+        throw new Exception("El valor ingresado no es válido.");
     }
 }
---
+++ SimplePayroll/Program.cs
@@ -0,0 +1,32 @@
+﻿using Shared;
+
+var response = string.Empty;
+do
+{
+    try
+    {
+        var name = ConsoleExtension.GetString          ("Ingrese el nombre del empleado..........: ");
+        var workedHours = ConsoleExtension.GetFloat    ("Ingrese las horas trabajadas............: ");
+        var hourValue = ConsoleExtension.GetDecimal    ("Ingrese el valor por hora...............: ");
+        var salaryMinimum = ConsoleExtension.GetDecimal("Ingrese el salario mínimo mensual.......: ");
+
+        var salary = (decimal)workedHours * hourValue;
+        if (salary < salaryMinimum)
+        {
+            Console.WriteLine($"Nombre...............................: {name}");
+            Console.WriteLine($"Salario..............................: {salaryMinimum:C2}");
+        }
+        else
+        {
+            Console.WriteLine($"Nombre..................................: {name}");
+            Console.WriteLine($"Salario.................................: {salary:C2}");    
+        }                
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    Console.Write("¿Desea continuar? (S/N): ");
+    response = Console.ReadLine()!.ToUpper(); 
+} while (response == "S");
+
