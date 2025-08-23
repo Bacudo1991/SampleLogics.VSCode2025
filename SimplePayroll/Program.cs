ebdbe26b98d7a81b49ad76df8d80f814a6373937
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-08-22 19:40:55 -0500

    Complete exercice 6

--- SimplePayroll/Program.cs
+++ SimplePayroll/Program.cs
@@ -5,21 +5,21 @@ do
 {
     try
     {
-        var name = ConsoleExtension.GetString          ("Ingrese el nombre del empleado..........: ");
-        var workedHours = ConsoleExtension.GetFloat    ("Ingrese las horas trabajadas............: ");
-        var hourValue = ConsoleExtension.GetDecimal    ("Ingrese el valor por hora...............: ");
-        var salaryMinimum = ConsoleExtension.GetDecimal("Ingrese el salario mínimo mensual.......: ");
+        var name = ConsoleExtension.GetString("Ingrese el nombre del empleado....................: ");
+        var workedHours = ConsoleExtension.GetFloat("Ingrese las horas trabajadas......................: ");
+        var hourValue = ConsoleExtension.GetDecimal("Ingrese el valor por hora.........................: ");
+        var salaryMinimum = ConsoleExtension.GetDecimal("Ingrese el salario mínimo mensual.................: ");
 
         var salary = (decimal)workedHours * hourValue;
         if (salary < salaryMinimum)
         {
-            Console.WriteLine($"Nombre...............................: {name}");
-            Console.WriteLine($"Salario..............................: {salaryMinimum:C2}");
+            Console.WriteLine($"Nombre............................................: {name}");
+            Console.WriteLine($"Salario...........................................: {salaryMinimum:C2}");
         }
         else
         {
-            Console.WriteLine($"Nombre..................................: {name}");
-            Console.WriteLine($"Salario.................................: {salary:C2}");    
+            Console.WriteLine($"Nombre............................................: {name}");
+            Console.WriteLine($"Salario...........................................: {salary:C2}");    
         }                
     }
     catch (Exception ex)
