18c444369d4c0cc6d6da0c1212e372f3e22e37eb
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-08-22 20:09:03 -0500

    Complete exercice 7

---
+++ Discounts/Program.cs
@@ -0,0 +1,38 @@
+﻿using Shared;
+
+var response = string.Empty;
+do
+{
+    try
+    {
+        var desks = ConsoleExtension.GetInt("Número de escritorios: ");
+        var valueToPay = CalculateValue(desks);
+
+        Console.WriteLine($"El valor a pagar es..: {valueToPay:C2}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    Console.Write("¿Desea continuar? (S/N): ");
+    response = Console.ReadLine()!.ToUpper(); 
+} while (response == "S");
+
+decimal CalculateValue(int desks)
+{
+        float discount;
+        if (desks < 5)
+        {
+            discount = 0.1f;
+        }
+        else if (desks < 10)
+        {
+            discount = 0.2f;
+        }
+        else
+        {
+            discount = 0.4f;
+        }
+        
+        return desks * 650000 * (decimal)(1 - discount);
+}
