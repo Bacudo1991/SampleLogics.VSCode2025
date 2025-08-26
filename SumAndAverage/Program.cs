63ced03968a2f6295d7c497f83b787fcb8583c1a
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-08-26 17:56:28 -0500

    Complete exercice 12

---
+++ SumAndAverage/Program.cs
@@ -0,0 +1,28 @@
+﻿using Shared;
+var response = string.Empty;
+do
+{
+    try
+    {
+        var n = ConsoleExtension.GetInt("Cuantos números desea: ");
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write($"{i}\t");
+            sum += i;
+        }
+        var average = sum / n;
+        Console.WriteLine();
+        Console.WriteLine($"La suma es: {sum,20:N0}");
+        Console.WriteLine($"El promedio es: {average,20:N0}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+
+    Console.Write("¿Desea continuar? (S/N): ");
+    response = Console.ReadLine()!.ToUpper();
+} while (response == "S");
+Console.WriteLine("\n:::::::::: GAME OVER ::::::::::\n");
