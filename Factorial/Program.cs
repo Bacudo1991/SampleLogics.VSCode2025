de11111bded3eb90a1d928a2cc14cf55c99142bc
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-08-26 19:23:20 -0500

    Complete exercice 13

---
+++ Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using Shared;
+var response = string.Empty;
+do
+{
+    try
+    {
+        var n = ConsoleExtension.GetInt("Ingrese número: ");
+        var factorial = MyMath.Factorial(n);
+        Console.WriteLine($"{n}! = {factorial:N0}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+
+    Console.Write("¿Desea continuar? (S/N): ");
+    response = Console.ReadLine()!.ToUpper();
+} while (response == "S");
+Console.WriteLine("\n:::::::::: GAME OVER ::::::::::\n");
