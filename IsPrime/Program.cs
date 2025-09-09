0260c5706bac1bb6ef45a5d551b86ee163c6a01e
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-09-09 12:50:50 -0500

    Complete exercice 19

---
+++ IsPrime/Program.cs
@@ -0,0 +1,25 @@
+﻿using Shared;
+var response = string.Empty;
+do
+{
+    try
+    {
+        var n = ConsoleExtension.GetInt("Ingrese número: ");
+        var isPrime = MyMath.IsPrime(n);
+        Console.WriteLine($"El número: {n}, {(isPrime ? "si" : "no")} es primo");
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    Console.Write("¿Desea continuar? (S/N): ");
+    response = Console.ReadLine()!.ToUpper();
+} while (response == "S");
+
+Console.WriteLine("\n :::::::::: GAME OVER :::::::::: \n");
+
+
+
+
