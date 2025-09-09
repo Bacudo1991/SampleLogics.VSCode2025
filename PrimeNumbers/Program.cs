ed588b41bc3b4ea3108bd340f037fd7ecae381c5
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-09-09 13:15:55 -0500

    Complete exercice 20

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using Shared;
+var response = string.Empty;
+do
+{
+    try
+    {
+        var n = ConsoleExtension.GetInt("Cuantos números primos quieres: ");
+        var primes = GetPrimes(n);
+        foreach (var prime in primes)
+        {
+            Console.Write($"{prime,10:N0}");
+        }
+        Console.WriteLine();
+        Console.WriteLine($"La sumatoria es: {primes.Sum(),10:N0}");
+        Console.WriteLine($"El promedio es: {primes.Average(),10:N0}");
+        Console.WriteLine();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    Console.Write("¿Desea continuar? (S/N): ");
+    response = Console.ReadLine()!.ToUpper();
+} while (response == "S");
+
+Console.WriteLine("\n :::::::::: GAME OVER :::::::::: \n");
+
+List<int> GetPrimes(int n)
+{
+    var primes = new List<int>();
+    var num = 2;
+    while (primes.Count < n)
+    {
+        if (MyMath.IsPrime(num))
+        {
+            primes.Add(num);
+        }
+        num++;
+    }
+    return primes;
+}
+
+
+
+
