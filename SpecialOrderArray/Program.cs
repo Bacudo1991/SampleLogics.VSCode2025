eead91ecd2fdd394ddc8fc898bb301dcaad9a1be
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-09-12 15:31:56 -0500

    Complete exercice 26

--- SpecialOrderArray/Program.cs
+++ SpecialOrderArray/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Globalization;
+using System.Runtime.InteropServices;
 using Shared;
 var response = string.Empty;
 do
@@ -13,11 +14,22 @@ do
 
         FillArray(numbers);
 
-
         Console.BackgroundColor = ConsoleColor.Red;
         Console.ForegroundColor = ConsoleColor.Black;
 
+        Console.WriteLine("Arreglo sin ordenar => ");
         ShowArray(numbers);
+        Console.WriteLine();
+
+        var numbersEven = GetNumbersEven(numbers);
+        var numbersOdd = GetNumbersOdd(numbers);
+        OrderArray(numbersEven, true);
+        OrderArray(numbersOdd);
+
+        Console.WriteLine("Arreglo ordenado  => ");
+        ShowArray(numbersEven);
+        ShowArray(numbersOdd);
+        Console.WriteLine();
 
         Console.BackgroundColor = ConsoleColor.Black;
         Console.ForegroundColor = ConsoleColor.Blue;
@@ -33,13 +45,94 @@ do
 
 Console.WriteLine("\n :::::::::: GAME OVER :::::::::: \n");
 
+int[] GetNumbersOdd(int[] numbers)
+{
+    var countOdds = 0;
+    foreach (var number in numbers)
+    {
+        if (!IsEven(number))
+        {
+            countOdds++;
+        }
+    }
+
+    var numbersOdd = new int[countOdds];
+    var i = 0;
+    foreach (var number in numbers)
+    {
+        if (!IsEven(number))
+        {
+            numbersOdd[i] = number;
+            i++;
+        }
+    }
+    return numbersOdd;
+}
+
+int[] GetNumbersEven(int[] numbers)
+{
+    var countEvens = 0;
+    foreach (var number in numbers)
+    {
+        if (IsEven(number))
+        {
+            countEvens++;
+        }
+    }
+
+    var numbersEven = new int[countEvens];
+    var i = 0;
+    foreach (var number in numbers)
+    {
+        if (IsEven(number))
+        {
+            numbersEven[i] = number;
+            i++;
+        }
+    }
+    return numbersEven;    
+}
+
+bool IsEven(int number) => number % 2 == 0;
+
+
+void OrderArray(int[] numbers, bool isDecending = false)
+{
+    for (int i = 0; i < numbers.Length - 1; i++)
+    {
+        for (int j = i + 1; j < numbers.Length; j++)
+        {
+            if (isDecending)
+            {
+                if (numbers[i] < numbers[j])
+                {
+                    Change(ref numbers[i], ref numbers[j]);
+                }
+            }
+            else
+            {
+                if (numbers[i] > numbers[j])
+                {
+                    Change(ref numbers[i], ref numbers[j]);
+                }
+            }
+        }
+    }
+}
+
+void Change(ref int number1, ref int number2)
+{
+    int aux = number1;
+    number1 = number2;
+    number2 = aux;  
+}
+
 void ShowArray(int[] numbers)
 {
     foreach (var number in numbers)
     {
         Console.Write($"{number,10:N0}");
     }
-    Console.WriteLine();
 }
 
 void FillArray(int[] numbers)
